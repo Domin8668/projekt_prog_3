a33cf1e7e0bf47ed148058dd66dd99616c886ea4
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-15 00:40:07 +0100

    Added basic scheme of cards and menubar.

--- Lista_Zakupow/src/ListaZakupow.java
+++ Lista_Zakupow/src/ListaZakupow.java
@@ -1,14 +1,49 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ListaZakupow {
     private JPanel mainPanel;
-    private JButton buttonTestowyButton;
+    private JPanel cardPanel;
+    private JMenuItem infoMenuItem;
+    private JMenuItem helpMenuItem;
+    private JMenuItem exitMenuItem;
+    private JPanel menuCard;
+    private JPanel inputCard;
+    private JPanel loadingCard;
+    private JPanel outputCard;
+    private JPanel infoCard;
+    private JPanel helpCard;
+    CardLayout cl = new CardLayout();
+
+    public ListaZakupow() {
+        cardPanel.setLayout(cl);
+        inputCard.setBackground(Color.green);
+        menuCard.setBackground(Color.yellow);
+        cardPanel.add(menuCard, "1");
+        cardPanel.add(inputCard, "2");
+        cl.show(cardPanel, "1");
+        infoMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cl.show(cardPanel, "2");
+            }
+        });
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Lista Zakupów");
         frame.setContentPane(new ListaZakupow().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(720, 480);
         frame.setVisible(true);
     }
 }
