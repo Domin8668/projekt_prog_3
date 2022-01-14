bf7b619bc1c948ba3486c6531f631ba9acfec757
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-14 22:16:34 +0100

    Added bare classes with runnable form.

--- Lista_Zakupow/src/ListaZakupow.java
+++ Lista_Zakupow/src/ListaZakupow.java
@@ -1,3 +1,13 @@
-public class ListaZakupow {
+import javax.swing.*;
 
+public class ListaZakupow {
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Lista Zakupów");
+        frame.setContentPane(new ListaZakupow().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
