d120ce4fef8f1cf91e6a80bd275bde597abee751
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2022-01-15 16:01:39 +0100

    Added SaveToFile implementation.

--- Lista_Zakupow/src/SaveToFile.java
+++ Lista_Zakupow/src/SaveToFile.java
@@ -1,2 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class SaveToFile {
+    public static void saveData(ArrayList<String> arr) throws FileNotFoundException {
+
+        PrintWriter outputFile = new PrintWriter("wyjscie.txt");
+
+        if(arr != null) {
+            for (String combination : arr) {
+                outputFile.println(combination);
+            }
+        }
+        else
+            System.out.println("Nie znaleziono pobranych danych z pliku wejściowego.");
+    }
 }
