0f42550dc2c94928c19a9138cd77c7e48a93b7cd
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-15 18:47:00 +0100

    Cleared the code and added calculateButton.

--- Lista_Zakupow/src/ListaZakupow.java
+++ Lista_Zakupow/src/ListaZakupow.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
@@ -23,15 +24,17 @@ public class ListaZakupow {
     private JLabel clockLabel;
     private JButton chooseFilesButton;
     private JLabel infoLabel;
-    private JLabel pomocLabel;
+    private JLabel helpLabel;
+    private JTextArea inputTextArea;
+    private JButton calculateButton;
+    private JScrollPane inputScrollPane;
     private File[] files;
     private ArrayList<String> input;
+    private ArrayList<String> result;
     CardLayout cl = new CardLayout();
 
     public ListaZakupow() {
         cardPanel.setLayout(cl);
-        inputCard.setBackground(Color.green);
-        menuCard.setBackground(Color.yellow);
         cardPanel.add(menuCard, "1");
         cardPanel.add(inputCard, "2");
         cardPanel.add(loadingCard, "3");
@@ -43,48 +46,63 @@ public class ListaZakupow {
         startClock();
 
         mainMenuItem.addActionListener(e -> cl.show(cardPanel, "1"));
-        infoMenuItem.addActionListener(e -> cl.show(cardPanel, "5"));
-        helpMenuItem.addActionListener(e -> cl.show(cardPanel, "6"));
+        infoMenuItem.addActionListener(e -> {
+            infoLabel.setText("""
+            <html>
+            Aplikacja "Lista zakupów" napisana w języku Java<br><br>
+            Autorzy:<br>&emsp;&emsp;Kamil Giziński<br>&emsp;&emsp;Dominik Sigulski<br>&emsp;&emsp;Bartosz Rolnik<br><br>
+            Projekt z przedmiotu Programowanie III<br>
+            Wydział Matematyki Stosowanej, Informatyka, sem. III, rok 2021/22<br><br>
+            "Lista zakupów" to zadanie z konkursu "Algorytmion" z roku 2012,<br>
+            które polega na znalezieniu takich kombinacji produktów,<br>
+            aby suma ich wartości odpowiadała budżetowi jaki mamy do rozdysponowania.
+            </html>""");
+            cl.show(cardPanel, "5");
+        });
+        helpMenuItem.addActionListener(e -> {
+            helpLabel.setText("""
+                <html>
+                Do wyboru z głównego panelu menu mamy następujące opcje:<br><br>
+                &emsp;&emsp;1. Menu główne<br>&emsp;&emsp;2. Informacje<br>&emsp;&emsp;3. Pomoc<br>&emsp;&emsp;4. Wyjście<br><br>
+                Opcja 1:<br>
+                &emsp;&emsp;W menu głównym możemy podjąc decyzję o tym jakie pliki chcemy analizować<br>
+                &emsp;&emsp;klikając przycisk "Wybierz pliki". Następnie poprzez naciśnięcie kolejnego przycisku<br>
+                &emsp;&emsp;program analizuje dane i zwraca wynik na ekran oraz do pliku.<br>
+                Opcja 2:<br>
+                &emsp;&emsp;Wyświetlenie informacji na temat zadania, autorów oraz przedmiotu w zakresie którego<br>
+                &emsp;&emsp;realizowany był projekt.<br><br>
+                Opcja 3:<br>
+                &emsp;&emsp;Wyświetlenie informacji pomocniczych na temat opcji zawartych w aplikacji.<br><br>
+                Opcja 4:<br>
+                &emsp;&emsp;Zakończenie działania aplikacji.<br><br>
+                </html>""");
+            cl.show(cardPanel, "6");
+        });
         exitMenuItem.addActionListener(e -> System.exit(0));
+        calculateButton.addActionListener(e -> {
+            cl.show(cardPanel, "4"); // Tymczasowo przechodzimy od razu do output
+            Calculate c = new Calculate();
+            c.calculate(input);
+            result = c.result;
+            for(String s : result) {
+                System.out.println(s);
+            }
+        });
         chooseFilesButton.addActionListener(e -> {
-            chooseFiles();
             try {
+                chooseFiles();
                 input = LoadFromFile.getInput(files);
+                StringBuilder inputToDisplay = new StringBuilder();
+                for(int i = 0; i < files.length; i++) {
+                    inputToDisplay.append("Plik ").append(files[i].getName()).append("\n");
+                    inputToDisplay.append(input.get(i)).append("\n\n");
+                }
+                inputTextArea.setText(String.valueOf(inputToDisplay));
+                cl.show(cardPanel, "2");
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-//            for(String s: input) {
-//                System.out.println(s);
-//            }
         });
-
-        // infoLabel
-        infoLabel.setText("<html>"
-            + "Aplikacja \"Lista zakupów\" napisana w języku Java<br><br>"
-            + "Autorzy:<br>&emsp;&emsp;Kamil Giziński<br>&emsp;&emsp;Dominik Sigulski<br>&emsp;&emsp;Bartosz Rolnik<br><br>"
-            + "Projekt z przedmiotu Programowanie III<br>"
-            + "Wydział Matematyki Stosowanej, Informatyka, sem. III, rok 2021/22<br><br>"
-            + "\"Lista zakupów\" to zadanie z konkursu \"Algorytmion\" z roku 2012,<br>"
-            + "które polega na znalezieniu takich kombinacji produktów,<br>"
-            + "aby suma ich wartości odpowiadała budżetowi jaki mamy do rozdysponowania."
-            + "</html>");
-
-        // pomocLabel
-        pomocLabel.setText("<html>"
-                + "Do wyboru z głównego panelu menu mamy następujące opcje:<br><br>"
-                + "&emsp;&emsp;1. Menu główne<br>&emsp;&emsp;2. Informacje<br>&emsp;&emsp;3. Pomoc<br>&emsp;&emsp;4. Wyjście<br><br>"
-                + "Opcja 1:<br>"
-                + "&emsp;&emsp;W menu głównym możemy podjąc decyzję o tym jakie pliki chcemy analizować<br>"
-                + "&emsp;&emsp;klikając przycisk \"Wybierz pliki\". Następnie poprzez naciśnięcie kolejnego przycisku<br>"
-                + "&emsp;&emsp;program analizuje dane i zwraca wynik na ekran oraz do pliku.<br>"
-                + "Opcja 2:<br>"
-                + "&emsp;&emsp;Wyświetlenie informacji na temat zadania, autorów oraz przedmiotu w zakresie którego<br>"
-                + "&emsp;&emsp;realizowany był projekt.<br><br>"
-                + "Opcja 3:<br>"
-                + "&emsp;&emsp;Wyświetlenie informacji pomocniczych na temat opcji zawartych w aplikacji.<br><br>"
-                + "Opcja 4:<br>"
-                + "&emsp;&emsp;Zakończenie działania aplikacji.<br><br>"
-                + "</html>");
     }
 
     public void startClock() {
@@ -102,9 +120,6 @@ public class ListaZakupow {
         int r = fc.showOpenDialog(mainPanel);
         if(r == JFileChooser.APPROVE_OPTION) {
             files = fc.getSelectedFiles();
-            for(File f: files) {
-                System.out.println(f.getName());
-            }
         }
     }
 
