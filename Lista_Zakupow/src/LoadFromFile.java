c7feb71b295ccf08a6011562902561ce4b0ab107
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-15 10:46:46 +0100

    Added todo and a basic method to expand.

--- Lista_Zakupow/src/LoadFromFile.java
+++ Lista_Zakupow/src/LoadFromFile.java
@@ -1,2 +1,19 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class LoadFromFile {
+    // TODO
+    // Dokończyć wczytywanie
+    // Tak, musi dostawać tablicę
+    // Więc biorąc to pod uwagę, będzie zwracać Listę stringów
+    // Każdy element listy to zawartość jednego pliku
+    // Obsługę błędów chyba zrobię z poziomu GUI, żeby wyświetlało ostrzeżenie i zamykało program
+    public static ArrayList<String> getInput(File[] files) {
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("1");
+        arr.add("1");
+        arr.add("1");
+        arr.add("1");
+        return arr;
+    }
 }
