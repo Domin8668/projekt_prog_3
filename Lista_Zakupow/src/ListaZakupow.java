8d92cb71aff17bc2056a4e6d65b3d238ad6ff5ab
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-15 10:45:50 +0100

    Added clock and full menu bar functionality.

--- Lista_Zakupow/src/ListaZakupow.java
+++ Lista_Zakupow/src/ListaZakupow.java
@@ -1,7 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
 import javax.swing.filechooser.*;
 
 public class ListaZakupow {
@@ -14,34 +16,63 @@ public class ListaZakupow {
     private JPanel inputCard;
     private JPanel loadingCard;
     private JPanel outputCard;
+    private JMenuItem mainMenuItem;
     private JPanel infoCard;
     private JPanel helpCard;
+    private JLabel clockLabel;
+    private JButton chooseFilesButton;
+    private File[] files;
+    private ArrayList<String> input;
     CardLayout cl = new CardLayout();
 
     public ListaZakupow() {
         cardPanel.setLayout(cl);
         inputCard.setBackground(Color.green);
         menuCard.setBackground(Color.yellow);
+        infoCard.setBackground(Color.blue);
+        helpCard.setBackground(Color.red);
         cardPanel.add(menuCard, "1");
         cardPanel.add(inputCard, "2");
+        cardPanel.add(loadingCard, "3");
+        cardPanel.add(outputCard, "4");
+        cardPanel.add(infoCard, "5");
+        cardPanel.add(helpCard, "6");
         cl.show(cardPanel, "1");
-        infoMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(cardPanel, "2");
-                FileFilter txtFileFilter = new FileNameExtensionFilter("Text File","txt");
-                JFileChooser fc = new JFileChooser();
-                fc.setAcceptAllFileFilterUsed(false);
-                fc.setFileFilter(txtFileFilter);
-                fc.showOpenDialog(mainPanel);
+
+        startClock();
+
+        mainMenuItem.addActionListener(e -> cl.show(cardPanel, "1"));
+        infoMenuItem.addActionListener(e -> cl.show(cardPanel, "5"));
+        helpMenuItem.addActionListener(e -> cl.show(cardPanel, "6"));
+        exitMenuItem.addActionListener(e -> System.exit(0));
+        chooseFilesButton.addActionListener(e -> {
+            chooseFiles();
+            input = LoadFromFile.getInput(files);
+            for(String s: input) {
+                System.out.println(s);
             }
         });
-        exitMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+    }
+
+    public void startClock() {
+        ActionListener updateClockAction = e -> clockLabel.setText(((new Date().toString()).substring(11, 19) + "    "));
+        Timer t = new Timer(100, updateClockAction);
+        t.start();
+    }
+
+    public void chooseFiles() {
+        JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        FileFilter txtFileFilter = new FileNameExtensionFilter("Pliki tekstowe","txt");
+        fc.setMultiSelectionEnabled(true);
+        fc.setAcceptAllFileFilterUsed(false);
+        fc.setFileFilter(txtFileFilter);
+        int r = fc.showOpenDialog(mainPanel);
+        if(r == JFileChooser.APPROVE_OPTION) {
+            files = fc.getSelectedFiles();
+            for(File f: files) {
+                System.out.println(f.getName());
             }
-        });
+        }
     }
 
     public static void main(String[] args) {
@@ -52,5 +83,9 @@ public class ListaZakupow {
         frame.setSize(720, 480);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+
     }
 }
